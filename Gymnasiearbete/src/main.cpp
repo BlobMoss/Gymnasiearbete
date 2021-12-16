e80d407c26cc4b89dd5bc19e9f76900fbb0600e4
Globfish <melvinbentinger2@gmail.com>
2021-12-16 23:03:22 +0100

    started networking

--- Gymnasiearbete/src/main.cpp
+++ Gymnasiearbete/src/main.cpp
@@ -2,6 +2,11 @@
 #include <string>
 #include <sstream>
 
+#include <chrono>
+
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif
 #define ASIO_STANDALONE
 #include <asio.hpp>
 #include <asio/ts/buffer.hpp>
@@ -23,6 +28,52 @@
 
 int main(void)
 {
+    // Asio
+
+    asio::error_code ec;
+
+    asio::io_context context;
+
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("185.195.92.80", ec), 80);
+
+    asio::ip::tcp::socket socket(context);
+
+    socket.connect(endpoint, ec);
+
+    if (!ec)
+    {
+        std::cout << "Connected!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Failed to conntect to adress:\n" << ec.message() << std::endl;
+    }
+
+    if (socket.is_open())
+    {
+        std::string sRequest =
+            "GET /index.html HTTP/1.1\r\n"
+            "Host: example.com\r\n"
+            "Connection: close\r\n\r\n";
+
+        socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
+
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(200ms);
+
+        size_t bytes = socket.available();
+        std::cout << "Bytes Available: " << bytes << std::endl;
+
+        if (bytes > 0)
+        {
+            std::vector<char> vBuffer(bytes);
+            socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
+
+            for (auto c : vBuffer)
+                std::cout << c;
+        }
+    }
+
     // Initialize GLFW
     if (!glfwInit())
         return -1;
